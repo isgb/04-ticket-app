c003b6b819114be2f5063feb02725fa5b968f6aa
isaias <isaias.gaona@zentro.com.mx>
2025-02-04 16:05:42 -0600

    avances

--- src/pages/Escritorio.js
+++ src/pages/Escritorio.js
@@ -1,7 +1,18 @@
+import { Col, Row, Typography } from 'antd'
 import React from 'react'
+
+const { Title, Text } = Typography
 
 export const Escritorio = () => {
   return (
-    <div>Escritorio</div>
+    <>
+      <Row>
+        <Col span={20}>
+          <Title level={2}>Escritorio</Title>
+          <Text>Usted está trabajando en el escritorio</Text>
+          <Text type="success">4</Text>
+        </Col>
+      </Row>
+    </>
   )
 }
--- src/pages/Ingresar.js
+++ src/pages/Ingresar.js
@@ -1,68 +1,85 @@
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Checkbox, Divider, Form, Input, InputNumber, Typography } from 'antd';
+import { SaveOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router';
 
-const onFinish = (values) => {
-  console.log('Success:', values);
-};
-const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
+const { Title, Text } = Typography;
 
 export const Ingresar = () => {
+
+  const navigate = useNavigate();
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+
+    navigate('/escritorio');  
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  
   return (
-    <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      style={{
-        maxWidth: 600,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Nombre el Agente"
-        name="agente"
-        rules={[
-          {
-            required: true,
-            message: 'Por favor ingrese su nombre',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <>
+      <Title level={2}>Ingresar</Title>
+      <Text>Ingrese su nombre y número de escritorio</Text>
+      <Divider />
 
-      <Form.Item
-        label="Escritorio"
-        name="escritorio"
-        rules={[
-          {
-            required: true,
-            message: 'Ingrese el número de escritorio',
-          },
-        ]}
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="Nombre el Agente"
+          name="agente"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor ingrese su nombre',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item label={null}>
-        <Button 
-            type="primary" 
+        <Form.Item
+          label="Escritorio"
+          name="escritorio"
+          rules={[
+            {
+              required: true,
+              message: 'Ingrese el número de escritorio',
+            },
+          ]}
+        >
+          <InputNumber min={1} max={99} />
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button
+            type="primary"
             htmlType="submit"
             shape='round'
-        >
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          >
+            <SaveOutlined />
+            Ingresar
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   )
 }
